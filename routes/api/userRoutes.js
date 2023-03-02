48d6da5af3b6b1b643cbbcce2f55fa7321c5818e
mjmOrminh <duyminh2032000@gmail.com>
2023-03-02 14:25:47 +0700

    feat: Add migrate database

--- routes/api/userRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const userController = require('../controllers/userController');
-
-const router = express.Router();
-
-router.get('/:userId', userController.getUser);
-router.post('/', userController.createUser);
-
-module.exports = router;
